41acbea3b5f11f5c3ae6edeeadefe0868823e2ac
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-12-21 15:41:50 +0100

    request one email on console.log

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function (responseAPI) {
+        const result = responseAPI.data.response;
+        console.log(result);
+    } );
 
 const { createApp } = Vue;
 
